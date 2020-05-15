acb10abafe066fa4497abf6004e54a96933d9527
servin <samuel.ervin@wal-mart.com>
2020-05-15 15:30:21 -0500

    fix repo

---
+++ build/precache-manifest.2c76e9abd61356ca827ec0843332138c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7334eee1dce6fac03fca57440235b3",
+    "url": "/daily-fishing-log/index.html"
+  },
+  {
+    "revision": "4cbe63bb1c94e3898b7b",
+    "url": "/daily-fishing-log/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36ddf7e3ff3ed1c4a933",
+    "url": "/daily-fishing-log/static/js/2.2a46def2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily-fishing-log/static/js/2.2a46def2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbe63bb1c94e3898b7b",
+    "url": "/daily-fishing-log/static/js/main.856745df.chunk.js"
+  },
+  {
+    "revision": "5003759ec3f09dcceac8",
+    "url": "/daily-fishing-log/static/js/runtime-main.4c4743de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daily-fishing-log/static/media/logo.5d5d9eef.svg"
+  }
+]);
